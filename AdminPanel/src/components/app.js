5af06850a44253d631b5c9093ae14025c76af3e3
Mehrdad Kamelzadeh <mehrdad.kamelzadeh@gmail.com>
2018-06-06 21:26:43 +0430

    RulesList added

--- AdminPanel/src/components/app.js
+++ AdminPanel/src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import RuleForm from './ruleForm';
+import RulesList from './rulesList';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <RuleForm/>
+        <RulesList/>
       </div>
     );
   }
--- AdminPanel/src/reducers/index.js
+++ AdminPanel/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import RulesReducer from '../reducers/rules_reducer';
 
 const rootReducer = combineReducers({
-  
+    rules: RulesReducer
 });
 
 export default rootReducer;
